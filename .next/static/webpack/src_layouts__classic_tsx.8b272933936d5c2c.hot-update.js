c30c2c842f8ba688e4da818d4da3bcce83777bd3
Knight-101 <sanskarmohapatra@gmail.com>
2023-10-19 00:28:34 +0530

    inital ui

---
+++ .next/static/webpack/src_layouts__classic_tsx.8b272933936d5c2c.hot-update.js
@@ -0,0 +1,22 @@
+"use strict";
+/*
+ * ATTENTION: An "eval-source-map" devtool has been used.
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file with attached SourceMaps in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+self["webpackHotUpdate_N_E"]("src_layouts__classic_tsx",{
+
+/***/ "./src/assets/images/author.jpg":
+/*!**************************************!*\
+  !*** ./src/assets/images/author.jpg ***!
+  \**************************************/
+/***/ (function(__unused_webpack_module, __webpack_exports__, __webpack_require__) {
+
+eval(__webpack_require__.ts("__webpack_require__.r(__webpack_exports__);\n/* harmony default export */ __webpack_exports__[\"default\"] = ({\"src\":\"/_next/static/media/author.67e99d23.jpg\",\"height\":160,\"width\":160,\"blurDataURL\":\"/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fauthor.67e99d23.jpg&w=8&q=70\",\"blurWidth\":8,\"blurHeight\":8});//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiLi9zcmMvYXNzZXRzL2ltYWdlcy9hdXRob3IuanBnLmpzIiwibWFwcGluZ3MiOiI7QUFBQSwrREFBZSxDQUFDLGdNQUFnTSIsInNvdXJjZXMiOlsid2VicGFjazovL19OX0UvLi9zcmMvYXNzZXRzL2ltYWdlcy9hdXRob3IuanBnPzQ1OGYiXSwic291cmNlc0NvbnRlbnQiOlsiZXhwb3J0IGRlZmF1bHQge1wic3JjXCI6XCIvX25leHQvc3RhdGljL21lZGlhL2F1dGhvci42N2U5OWQyMy5qcGdcIixcImhlaWdodFwiOjE2MCxcIndpZHRoXCI6MTYwLFwiYmx1ckRhdGFVUkxcIjpcIi9fbmV4dC9pbWFnZT91cmw9JTJGX25leHQlMkZzdGF0aWMlMkZtZWRpYSUyRmF1dGhvci42N2U5OWQyMy5qcGcmdz04JnE9NzBcIixcImJsdXJXaWR0aFwiOjgsXCJibHVySGVpZ2h0XCI6OH07Il0sIm5hbWVzIjpbXSwic291cmNlUm9vdCI6IiJ9\n//# sourceURL=webpack-internal:///./src/assets/images/author.jpg\n"));
+
+/***/ })
+
+});
---
+++ src/components/create-nft/create-nft.tsx
@@ -0,0 +1,215 @@
+import { useState } from 'react';
+import cn from 'classnames';
+import { NextSeo } from 'next-seo';
+import { Transition } from '@/components/ui/transition';
+import { RadioGroup } from '@/components/ui/radio-group';
+import { Listbox } from '@/components/ui/listbox';
+import Image from '@/components/ui/image';
+import Button from '@/components/ui/button';
+import { Switch } from '@/components/ui/switch';
+import Input from '@/components/ui/forms/input';
+import Textarea from '@/components/ui/forms/textarea';
+import Uploader from '@/components/ui/forms/uploader';
+import InputLabel from '@/components/ui/input-label';
+import ToggleBar from '@/components/ui/toggle-bar';
+import { ChevronDown } from '@/components/icons/chevron-down';
+import { Ethereum } from '@/components/icons/ethereum';
+import { Flow } from '@/components/icons/flow';
+import { Warning } from '@/components/icons/warning';
+import { Unlocked } from '@/components/icons/unlocked';
+import Avatar from '@/components/ui/avatar';
+//images
+import AuthorImage from '@/assets/images/author.jpg';
+import NFT1 from '@/assets/images/nft/nft-1.jpg';
+import ClaimType from '@/components/create-nft/price-types-props';
+
+const BlockchainOptions = [
+  {
+    id: 1,
+    name: 'SUI',
+    value: 'sui',
+    icon: <Ethereum />,
+  },
+
+];
+
+export default function CreateNFT() {
+  let [publish, setPublish] = useState(true);
+  let [explicit, setExplicit] = useState(false);
+  let [unlocked, setUnlocked] = useState(false);
+  let [claimType, setClaimType] = useState('self');
+  let [blockchain, setBlockChain] = useState(BlockchainOptions[0]);
+  return (
+    <>
+      <div className="mx-auto w-full sm:pt-0 lg:px-8 xl:px-10 2xl:px-0">
+        <h2 className="mb-6 text-lg font-medium uppercase tracking-wider text-gray-900 dark:text-white sm:mb-10 sm:text-2xl">
+          UNDERWRITE NEW POLICY
+        </h2>
+        <div className="mb-8 grid grid-cols-1 gap-12 lg:grid-cols-3">
+          <div className="lg:col-span-2">
+            {/* File uploader */}
+            <div className="mb-8">
+              <InputLabel title="Upload file" important />
+              <Uploader />
+            </div>
+
+            {/* NFT price type */}
+
+            <InputLabel
+              title="CLAIMS & DISBURSAL PROCESSOR"
+              subTitle="Choose how you wish to enable claims filling,approval and disbursal for your policy"
+            />
+
+            <ClaimType value={claimType} onChange={setClaimType} />
+          </div>
+
+          {/* <div className="hidden flex-col lg:flex">
+            {/* NFT preview */}
+          {/* <InputLabel title="Preview" />
+            <div className="relative flex flex-grow flex-col overflow-hidden rounded-lg bg-white shadow-card transition-all duration-200 hover:shadow-large dark:bg-light-dark">
+              <div className="flex items-center p-4 text-sm font-medium text-gray-600 transition hover:text-gray-900 dark:text-gray-400">
+                <Avatar
+                  size="sm"
+                  image={AuthorImage}
+                  alt="Cameronwilliamson"
+                  className="border-white bg-gray-300 ltr:mr-3 rtl:ml-3 dark:bg-gray-400"
+                />
+                @Cameronwilliamson
+              </div>
+              <div className="relative block w-full pb-full">
+                <Image
+                  src={NFT1}
+                  placeholder="blur"
+                  layout="fill"
+                  objectFit="cover"
+                  alt="Pulses of Imagination #214"
+                />
+              </div>
+              <div className="p-5">
+                <div className="text-sm font-medium text-black dark:text-white">
+                  Pulses Of Imagination #214
+                </div>
+                <div className="mt-4 text-lg font-medium text-gray-900 dark:text-white">
+                  0.40 ETH
+                </div>
+              </div>
+            </div>
+          </div> */}
+        </div>
+
+        {/* Price */}
+        <div className="mb-8">
+          <InputLabel title="Premium Amount" important />
+          <Input
+            min={0}
+            type="number"
+            placeholder="Enter your price"
+            inputClassName="spin-button-hidden"
+          />
+        </div>
+
+        {/* Name */}
+        <div className="mb-8">
+          <InputLabel title="Policy Expiry" important />
+          <Input type="text" placeholder="Item name" />
+        </div>
+
+        {/* External link */}
+        <div className="mb-8">
+          <InputLabel
+            title="Coverage Amount"
+            subTitle="Mention the maximum coverage amount of this policy (mentioned at the time of sales) along with maximum amount per claim, maximum amount for copay and so on."
+          />
+          <Input type="text" placeholder="https://yoursite.io/item/123" />
+        </div>
+
+        {/* Description */}
+        <div className="mb-8">
+          <InputLabel
+            title="Coverage Details"
+            subTitle="Itemized details on coverage specifics with payment limit per billable item, disbursal conditions, payout schedules, dispute resolution and jurisdiction information."
+          />
+          <Textarea placeholder="Provide a detailed description of your item" />
+        </div>
+
+        {/* Unlockable content */}
+        <div className="mb-3">
+          <ToggleBar
+            title="RISK MITIGATION"
+            subTitle="Enforce recommended best practices for portfolio risk mitigation such as allowing people to create third-party price oracles for your token."
+            icon={<Unlocked />}
+            checked={unlocked}
+            onChange={() => setUnlocked(!unlocked)}
+          >
+            {unlocked && (
+              <Textarea placeholder="Enter content" />
+            )}
+          </ToggleBar>
+        </div>
+
+        {/* Explicit content */}
+        <div className="mb-8">
+          <ToggleBar
+            title="DYNAMIC PAYOUTS ON EXPIRY"
+            subTitle="Set the payout method of non-disbursed premiums for expired policies"
+            icon={<Warning />}
+            checked={explicit}
+            onChange={() => setExplicit(!explicit)}
+          />
+        </div>
+
+        {/* Supply */}
+        <div className="mb-8">
+          <InputLabel
+            title="Supply"
+            subTitle="The total number of tokens to be minted. Recommended: Keep number of tokens same as coverage amount in US$"
+          />
+          <Input type="number" placeholder="1" disabled />
+        </div>
+
+        {/* Blockchain */}
+        <div className="mb-8">
+          <InputLabel title="Blockchain" />
+          <div className="relative">
+            <Listbox value={blockchain} onChange={setBlockChain}>
+              <Listbox.Button className="text-case-inherit letter-space-inherit flex h-10 w-full items-center justify-between rounded-lg border border-gray-200 bg-white px-4 text-sm font-medium text-gray-900 outline-none transition-shadow duration-200 hover:border-gray-900 hover:ring-1 hover:ring-gray-900 dark:border-gray-700 dark:bg-gray-800 dark:text-gray-100 dark:hover:border-gray-600 dark:hover:ring-gray-600 sm:h-12 sm:px-5">
+                <div className="flex items-center">
+                  <span className="ltr:mr-2 rtl:ml-2">{blockchain.icon}</span>
+                  {blockchain.name}
+                </div>
+                <ChevronDown />
+              </Listbox.Button>
+              <Transition
+                leave="transition ease-in duration-100"
+                leaveFrom="opacity-100"
+                leaveTo="opacity-0"
+              >
+                <Listbox.Options className="absolute left-0 z-10 mt-1 grid w-full origin-top-right gap-0.5 rounded-lg border border-gray-200 bg-white p-1 shadow-large outline-none dark:border-gray-700 dark:bg-gray-800 xs:p-2">
+                  {BlockchainOptions.map((option) => (
+                    <Listbox.Option key={option.id} value={option}>
+                      {({ selected }) => (
+                        <div
+                          className={`flex cursor-pointer items-center rounded-md px-3 py-2 text-sm text-gray-900 transition dark:text-gray-100  ${selected
+                            ? 'bg-gray-200/70 font-medium dark:bg-gray-600/60'
+                            : 'hover:bg-gray-100 dark:hover:bg-gray-700/70'
+                            }`}
+                        >
+                          <span className="ltr:mr-2 rtl:ml-2">
+                            {option.icon}
+                          </span>
+                          {option.name}
+                        </div>
+                      )}
+                    </Listbox.Option>
+                  ))}
+                </Listbox.Options>
+              </Transition>
+            </Listbox>
+          </div>
+        </div>
+
+        <Button shape="rounded">UNDERWRITE NOW</Button>
+      </div>
+    </>
+  );
+}
